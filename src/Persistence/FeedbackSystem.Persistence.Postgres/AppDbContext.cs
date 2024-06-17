a919f1392d8ba54942004a5ef47cec9368dafa6d
Katlego Mokhethi <mokhethikatlego@gmail.com>
2024-06-17 19:59:47 +0200

    Add Db context and factory

---
+++ src/Persistence/FeedbackSystem.Persistence.Postgres/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using FeedbackSystem.Persistence.Postgres.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FeedbackSystem.Persistence.Postgres;
+
+public class AppDbContext : DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base (options) { }
+
+    public DbSet<FeedbackEntity> Feedback { get; set; }
+}
---
+++ src/Persistence/FeedbackSystem.Persistence.Postgres/AppDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace FeedbackSystem.Persistence.Postgres;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseNpgsql("Host=localhost;Database=FeedbackSystemDb;Username=prosoftAdmin;Password=admin;");
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
