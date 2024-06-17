7790eb0ae4f4c66d1c5d4e1a36efd3ca3a07d5ed
Katlego Mokhethi <mokhethikatlego@gmail.com>
2024-06-17 20:00:22 +0200

    Implement Feedback Repository

---
+++ src/Persistence/FeedbackSystem.Persistence.Postgres/FeedbackRepository.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using FeedbackSystem.Domain.Interfaces;
+using FeedbackSystem.Domain.Models;
+using FeedbackSystem.Persistence.Postgres.Entities;
+using FeedbackSystem.Persistence.Postgres.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace FeedbackSystem.Persistence.Postgres;
+
+public class FeedbackRepository : IFeedbackRepository
+{
+    private IDbContextFactory<AppDbContext> _dbContextFactory;
+    private readonly IMapper _mapper;
+
+    public FeedbackRepository(IDbContextFactory<AppDbContext> dbContextFactory)
+    {
+        _dbContextFactory = dbContextFactory;
+
+        var mappingConfig = new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfile()));
+        _mapper = new Mapper(mappingConfig);
+    }
+
+    public async Task AddFeedbackAsync(Feedback feedback)
+    {
+        try
+        {
+            await using var dbContext = await _dbContextFactory.CreateDbContextAsync();
+
+            var entity = _mapper.Map<FeedbackEntity>(feedback);
+            entity.CreatedDatetime = DateTime.UtcNow;
+
+            await dbContext.Feedback.AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+        }
+        catch (Exception exception)
+        {
+            throw new Exception($"Could not add feedback by {feedback.CreatedBy}. {exception.Message}");
+        }
+    }
+}
