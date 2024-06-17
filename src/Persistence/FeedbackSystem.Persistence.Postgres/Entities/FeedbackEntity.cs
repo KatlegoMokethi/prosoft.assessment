de653a65446d97c6be87dcad36bc5f428a1df7fb
Katlego Mokhethi <mokhethikatlego@gmail.com>
2024-06-17 19:58:44 +0200

    Add feedback entity and relevant mapping profile

---
+++ src/Persistence/FeedbackSystem.Persistence.Postgres/Entities/FeedbackEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FeedbackSystem.Persistence.Postgres.Entities;
+
+[Table("feedback")]
+public class FeedbackEntity
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+    [Column("service")]
+    public string Service { get; set; } = string.Empty;
+    [Column("description")]
+    public string Description { get; set; } = string.Empty;
+    [Column("created_by")]
+    public string CreatedBy { get; set; } = string.Empty;
+    [Column("created_datetime")]
+    public DateTime CreatedDatetime { get; set; }
+}
---
+++ src/Persistence/FeedbackSystem.Persistence.Postgres/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FeedbackSystem.Domain.Models;
+using FeedbackSystem.Persistence.Postgres.Entities;
+
+namespace FeedbackSystem.Persistence.Postgres.Mappings;
+
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+        CreateMap<Feedback, FeedbackEntity>()
+            .ReverseMap();
+    }
+}
