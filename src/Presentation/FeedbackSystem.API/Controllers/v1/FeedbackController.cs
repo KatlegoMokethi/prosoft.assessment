e587de0ef3cc1b2c0b96a658c96317a5859c983d
Katlego Mokhethi <mokhethikatlego@gmail.com>
2024-06-18 21:21:35 +0200

    Add API versioning to controller(s)

---
+++ src/Presentation/FeedbackSystem.API/Controllers/v1/FeedbackController.cs
@@ -0,0 +1,25 @@
+﻿using Asp.Versioning;
+using FeedbackSystem.Application.Commands.SubmitFeedback;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeedbackSystem.API.Controllers.v1;
+
+[ApiVersion("1")]
+[Route("api/v{v:apiVersion}/feedback")]
+public class FeedbackController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public FeedbackController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [MapToApiVersion("1")]
+    [HttpPost]
+    public Task<string> Post([FromBody] SubmitFeedbackCommand command)
+    {
+        return _mediator.Send(command);
+    }
+}
