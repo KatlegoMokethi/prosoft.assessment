b1a559c0a1da53ad2d4d7ae757770dc40d1eddfd
Katlego Mokhethi <mokhethikatlego@gmail.com>
2024-06-18 23:54:11 +0200

    Add AuthenticationOptions to appsettings

---
+++ src/Presentation/FeedbackSystem.API/Configuration/AuthenticationOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace FeedbackSystem.API.Configuration;
+
+public class AuthenticationOptions
+{
+    public string UserId { get; set; } = string.Empty;
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+    public string SecurityKey { get; set; } = string.Empty;
+    public int ExpiryTimeInMinutes { get; set; }
+}
