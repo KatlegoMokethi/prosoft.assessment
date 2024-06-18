1a9cbd811e4c0f78d81da00a8711fd475cc85fe5
Katlego Mokhethi <mokhethikatlego@gmail.com>
2024-06-18 21:20:57 +0200

    Add API versioning filters

---
+++ src/Presentation/FeedbackSystem.API/Filters/RemoveVersionParameterFilter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace FeedbackSystem.API.Filters;
+
+public class RemoveVersionParameterFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        var vParam = operation.Parameters.SingleOrDefault(x => x.Name == "v");
+        operation.Parameters.Remove(vParam);
+    }
+}
+
---
+++ src/Presentation/FeedbackSystem.API/Filters/RemoveVersionWithValueFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace FeedbackSystem.API.Filters;
+
+public class RemoveVersionWithValueFilter : IDocumentFilter
+{
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        var paths = new OpenApiPaths();
+
+        foreach (var path in swaggerDoc.Paths)
+            paths.Add(path.Key.Replace("v{v}", swaggerDoc.Info.Version), path.Value);
+
+        swaggerDoc.Paths = paths;
+    }
+}
+
