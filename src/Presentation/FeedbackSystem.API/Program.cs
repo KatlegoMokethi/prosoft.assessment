2bce7053eacf95359048620452663293b68131ec
Katlego Mokhethi <mokhethikatlego@gmail.com>
2024-06-18 21:21:17 +0200

    Configure API versioning

--- src/Presentation/FeedbackSystem.API/Program.cs
+++ src/Presentation/FeedbackSystem.API/Program.cs
@@ -1,25 +1,63 @@
-﻿using FeedbackSystem.Application;
+﻿using Asp.Versioning;
+using FeedbackSystem.API.Filters;
+using FeedbackSystem.Application;
 using FeedbackSystem.Persistence.Postgres;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+ConfigureSwagger(builder.Services);
+ConfigureApiVersioning(builder.Services);
 
 builder.Services.AddApplicationLayer();
 builder.Services.AddPersistenceLayer(builder.Configuration.GetConnectionString("DbConnectionString")!);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
+
+void ConfigureSwagger(IServiceCollection services)
+{
+    services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Customer Feedback Management System",
+            Description = "Prosoft Customer Feedback Management System API",
+            TermsOfService = new Uri("https://prosoftit.wpengine.com/terms-of-use/"),
+            Contact = new OpenApiContact
+            {
+                Name = "Contact",
+                Url = new Uri("https://prosoft.dev/contact/")
+            }
+        });
+
+
+        options.OperationFilter<RemoveVersionParameterFilter>();
+        options.DocumentFilter<RemoveVersionWithValueFilter>();
+    });
+}
+
+void ConfigureApiVersioning(IServiceCollection services)
+{
+    services.AddApiVersioning(options =>
+    {
+        options.DefaultApiVersion = new ApiVersion(1);
+        options.ReportApiVersions = true;
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        options.ApiVersionReader = new UrlSegmentApiVersionReader();
+    }).AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'V";
+        options.SubstituteApiVersionInUrl = true;
+    });
+}
