477f8e05f213cebb1e19b5dc40abc9ed71c64bc8
Katlego Mokhethi <mokhethikatlego@gmail.com>
2024-06-19 08:09:04 +0200

    Send notifications after feedback submission

--- src/Core/FeedbackSystem.Application/Commands/SubmitFeedback/SubmitFeedbackHandler.cs
+++ src/Core/FeedbackSystem.Application/Commands/SubmitFeedback/SubmitFeedbackHandler.cs
@@ -3,17 +3,21 @@ using FeedbackSystem.Domain.Interfaces;
 using AutoMapper;
 using FeedbackSystem.Application.Mappings;
 using FeedbackSystem.Domain.Models;
+using FeedbackSystem.Infrastructure.Services;
+using FeedbackSystem.Infrastructure.Models;
 
 namespace FeedbackSystem.Application.Commands.SubmitFeedback
 {
 	public class SubmitFeedbackHandler : IRequestHandler<SubmitFeedbackCommand, string>
 	{
         private readonly IFeedbackRepository _feedbackRepo;
+        private readonly INotificationService _notificationService;
         private readonly IMapper _mapper;
 
-        public SubmitFeedbackHandler(IFeedbackRepository feedbackRepo)
+        public SubmitFeedbackHandler(IFeedbackRepository feedbackRepo, INotificationService notificationService)
 		{
             _feedbackRepo = feedbackRepo;
+            _notificationService = notificationService;
 
             var mappingConfig = new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfile()));
             _mapper = new Mapper(mappingConfig);
@@ -23,6 +27,13 @@ namespace FeedbackSystem.Application.Commands.SubmitFeedback
         {
             var feedback = _mapper.Map<Feedback>(request);
             await _feedbackRepo.AddFeedbackAsync(feedback);
+
+            await _notificationService.SendEmailAsync(new MailOptions
+            {
+                Subject = "Prosoft IT Feedback System Notification",
+                Body = $"New feedback was submitted for {request.Service}: {request.Description}"
+            });
+
             return "Thanks for your feedback!";
         }
     }
