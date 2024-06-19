7133bedee922a27f71db37d2b2bdd5be03322736
Katlego Mokhethi <mokhethikatlego@gmail.com>
2024-06-19 08:07:11 +0200

    Add custom notification service

---
+++ src/Infrastructure/FeedbackSystem.Infrastructure/Services/INotificationService.cs
@@ -0,0 +1,8 @@
+﻿using FeedbackSystem.Infrastructure.Models;
+
+namespace FeedbackSystem.Infrastructure.Services;
+
+public interface INotificationService
+{
+    Task SendEmailAsync(MailOptions mailRequest);
+}
---
+++ src/Infrastructure/FeedbackSystem.Infrastructure/Services/NotificationService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Mail;
+using FeedbackSystem.Infrastructure.Configuration;
+using FeedbackSystem.Infrastructure.Models;
+
+namespace FeedbackSystem.Infrastructure.Services;
+
+public class NotificationService : INotificationService
+{
+    readonly MailNotificationOptions _notificationOptions;
+
+    public NotificationService(MailNotificationOptions notificationOptions)
+    {
+        _notificationOptions = notificationOptions;
+    }
+
+    public async Task SendEmailAsync(MailOptions mailOptions)
+    {
+        var message = new MailMessage();
+        var smtp = new SmtpClient();
+
+        message.From = new MailAddress(_notificationOptions.FromAddress, _notificationOptions.DisplayName);
+        message.To.Add(string.Join(",", _notificationOptions.AdminEmails));
+        message.Subject = mailOptions.Subject;
+
+        message.IsBodyHtml = false;
+        message.Body = mailOptions.Body;
+
+        smtp.Port = _notificationOptions.Port;
+        smtp.Host = _notificationOptions.Host;
+        smtp.EnableSsl = true;
+        smtp.UseDefaultCredentials = false;
+        smtp.Credentials = new NetworkCredential(_notificationOptions.FromAddress, _notificationOptions.Password);
+        smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+        await smtp.SendMailAsync(message);
+    }
+}
