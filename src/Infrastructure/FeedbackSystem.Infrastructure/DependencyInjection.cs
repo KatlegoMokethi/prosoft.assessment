c959e10c31b8ec8434a723c6edd6a639bebcabd7
Katlego Mokhethi <mokhethikatlego@gmail.com>
2024-06-19 08:07:27 +0200

    Add DI for notification service

---
+++ src/Infrastructure/FeedbackSystem.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using FeedbackSystem.Infrastructure.Configuration;
+using FeedbackSystem.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace FeedbackSystem.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddNotificationService(this IServiceCollection services, IConfiguration config)
+    {
+        var notificationOptions = new MailNotificationOptions();
+        config.GetSection("MailNotificationOptions").Bind(notificationOptions);
+        services.AddSingleton(notificationOptions);
+
+        services.AddSingleton<INotificationService, NotificationService>();
+
+        return services;
+    }
+}
